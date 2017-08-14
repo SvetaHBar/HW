1a9608e530f560fef4dbd4af8519eac47e3ea198
SvetaHBar <svetahaas@gmail.com>
2017-08-14 09:59:51 +0300

    fix edy's comments

--- parcer-for-log2/Edy's_HW_3_Picnic_calc w_o debug mode.py
+++ parcer-for-log2/Edy's_HW_3_Picnic_calc w_o debug mode.py
@@ -1,29 +1,18 @@
 import json
 import sys
 
-Data_file = "C:\\Users\\svetah\\Desktop\\py\\parcer-for-log2\\picnic_list.json"
-#meat_per_adult = float(0.5)   #kg # params for hardcoded instead of json
-#meat_per_child = float(0.2)  #kg # params for hardcoded instead of json
-#bear_per_adult = int(2) #bottle # params for hardcoded instead of json
-#bear_per_child = int(0) # params for hardcoded instead of json
-#soft_drink_per_adult = float(0.5) #litr # params for hardcoded instead of json
-#soft_drink_per_child = float(0.5) #litr # params for hardcoded instead of json
-#meat_price_per_kg # params for hardcoded instead of json
-#bear_price_per_botle # params for hardcoded instead of json
-#drink_price_per_botle # params for hardcoded instead of json
+DATA_FILE = "C:\\Users\\svetah\\Desktop\\py\\parcer-for-log2\\picnic_list.json"
 
-
-
-elements_from_json = []  # empty dict for keep all relevant params from json file
 def read_from_file():
-    with open(Data_file, 'r') as basic_info:
-        read_data = json.load(basic_info)  #convert the json string to a dict
-        iner_dict = read_data['data_per_oreah'] #because of json is a list of lists , will use only params from data_per_oreh list
-        return iner_dict
+    with open(DATA_FILE, 'r') as f:
+        picnic_data = json.load(f)  #convert the json string to a dict, where f is basic_info
+        #iner_dict = read_data['data_per_oreah'] #because of json is a list of lists , will use only params from food_and_drinks list
+        #return iner_dict
+        return picnic_data['data_per_oreah']
 
 
-def is_food_drink_in_dict(iner_dict):
-    return iner_dict
+def is_food_drink_in_dict(picnic_data):
+    return picnic_data
 
 
 def enter_number_of_participants_adults():
@@ -34,24 +23,25 @@ def enter_number_of_participants_children():
     input_num_of_children = int( raw_input("Please enter num of children:"))
     return input_num_of_children
 
-def calculation(input_num_of_adults, input_num_of_children , iner_dict):
-        meat_for_adults = (input_num_of_adults * iner_dict['meat_per_adult'])
-        print('Meat for adults is :' , meat_for_adults, 'kg')
-        meat_for_children = input_num_of_children * iner_dict['meat_per_child']
-        print ("Meat for children is :" , meat_for_children, 'kg')
-        meat_total  = meat_for_adults + meat_for_children
-        print ("Total meat is: ", meat_total, 'kg')
-        bears_for_adults = (input_num_of_adults * iner_dict['bear_per_adult'])
-        print ("Total bears_for_adults is: ", bears_for_adults, 'bottle')
-        bears_for_children = (input_num_of_children * iner_dict['bear_per_child'])
-        print ("Total bears_for_children is: ", bears_for_children, 'bottle')
-        bears_total = bears_for_adults + bears_for_children
-        print ("Total bears is: ", bears_total, 'bottle')
-        softdrink_for_adults = (input_num_of_adults * iner_dict['soft_drink_per_adult'])
-        print('Soft Drink for adults is :', softdrink_for_adults, 'litr')
-        softdrink_for_children = input_num_of_children * iner_dict['soft_drink_per_child']
-        print ("Soft Drink for children is :", softdrink_for_children)
+def calculation(input_num_of_adults, input_num_of_children , picnic_data):
+        meat_for_adults = (input_num_of_adults * picnic_data['meat_per_adult'])
+        meat_for_children = input_num_of_children * picnic_data['meat_per_child']
+        meat_total = meat_for_adults + meat_for_children
+        bears_for_adults = (input_num_of_adults * picnic_data['bear_per_adult'])
+        bears_for_children = (input_num_of_children * picnic_data['bear_per_child'])
+        beers_total = bears_for_adults + bears_for_children
+        softdrink_for_adults = (input_num_of_adults * picnic_data['soft_drink_per_adult'])
+        softdrink_for_children = input_num_of_children * picnic_data['soft_drink_per_child']
         softdrink_total = softdrink_for_adults + softdrink_for_children
+
+        print('Meat for adults is :' , meat_for_adults, 'kg')
+        print ("Meat for children is :" , meat_for_children, 'kg')
+        print ("Total meat is: ", meat_total, 'kg')
+        print ("Total beers_for_adults is: ", bears_for_adults, 'bottle')
+        print ("Total beers_for_children is: ", bears_for_children, 'bottle')
+        print ("Total beers is: ", beers_total, 'bottle')
+        print('Soft Drink for adults is :', softdrink_for_adults, 'litr')
+        print ("Soft Drink for children is :", softdrink_for_children)
         print ("Total Soft Drink is :",softdrink_total)
 
 
@@ -59,7 +49,7 @@ def calculation(input_num_of_adults, input_num_of_children , iner_dict):
 
 def main():
     a =  read_from_file()
-    is_food_drink_in_dict(a)
+    #is_food_drink_in_dict(a)
     x = enter_number_of_participants_adults()  # meahsenim parametr 'num of adults' in x she machil returned ereh
     y = enter_number_of_participants_children()
     z = is_food_drink_in_dict(a)
